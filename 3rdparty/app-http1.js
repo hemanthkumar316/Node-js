eb5b6e13e4d5eaa1591274e55a08c4a251d988a5
hemanthkumar316 <hemanth118818@gmail.com>
2022-09-23 23:36:58 +0530

    done

---
+++ 3rdparty/app-http1.js
@@ -0,0 +1,11 @@
+const http=require('http')
+const dotenv =require('dotenv')
+dotenv.config({path:'config/config.env'})
+console.log(process.env.PORT)
+let server=http.createServer((req,res)=>{
+ res.writeHead(200,{'Content-type':'text/html'})
+ res.end('<h1> Hii This is Hemanth......</h1>')
+})
+server.listen(process.env.PORT,()=>{
+ console.log(`Server is Runing On PORT No :${process.env.PORT}`)
+})
---
+++ 3rdparty/app-url.js
@@ -0,0 +1,56 @@
+//static website routing using url module and http moodule to create server
+const http=require('http')
+const fs=require('fs')
+const path=require('path')
+let server=http.createServer((request,response)=>{
+ let url=request.url
+ console.log(url)
+ response.writeHead(200,{'Content-type':'text/html'})
+ if(url==='/'){
+  fs.readFile(path.join(__dirname,"static","index.html"),"utf-8",(err,data)=>{
+   if(err) throw err
+   response.end(data)
+  })
+ }
+ else  if(url==='/home'){
+  fs.readFile(path.join(__dirname,"static","index.html"),"utf-8",(err,data)=>{
+   if(err) throw err
+   response.end(data)
+  })
+ }
+ else  if(url==='/about'){
+  fs.readFile(path.join(__dirname,"static","about.html"),"utf-8",(err,data)=>{
+   if(err) throw err
+   response.end(data)
+  })
+ }
+
+ else  if(url==='/service'){
+  fs.readFile(path.join(__dirname,"static","service.html"),"utf-8",(err,data)=>{
+   if(err) throw err
+   response.end(data)
+  })
+ }
+ else  if(url==='/product'){
+  fs.readFile(path.join(__dirname,"static","product.html"),"utf-8",(err,data)=>{
+   if(err) throw err
+   response.end(data)
+  })
+ }
+ else  if(url==='/contact'){
+  fs.readFile(path.join(__dirname,"static","contact.html"),"utf-8",(err,data)=>{
+   if(err) throw err
+   response.end(data)
+  })
+ }
+ else {
+  fs.readFile(path.join(__dirname, 'static', '404.html'), 'utf-8', (err, data) => {
+      if (err) throw err
+      response.end(data)
+  })
+}
+})
+server.listen(8080,(err)=>{
+ if(err) throw err
+ console.log('server is running on port number :8080')
+})
